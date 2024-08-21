e70a889c7156168bec201d0c3d18c4688df6ca3b
AlexandraL9 <alexandramslima@gmail.com>
2024-08-21 20:21:07 -0300

    Projeto inicial

---
+++ LeiloesTDSat/src/conectaDAO.java
@@ -0,0 +1,23 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class conectaDAO {
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+        
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/uc11?user=alexandra&password=123456");
+            
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
